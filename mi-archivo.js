f1f096b2b8ddc8a3e0297fe3b002f742408586f6
m-nats99 <natsor99@gmail.com>
2026-01-31 11:02:19 -0600

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,3 +1,6 @@
 // Parte B Comentario: Este archivo imprime un mensaje en consol 
 console.log("Hola mundo");
 
+// Parte C: archivo editado
+console.log("Hola mundo_editado");
+console.log("cambios en el archivo");
